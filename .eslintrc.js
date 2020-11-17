ea2b4d7e1186b2d51c09af72201a18b4ac62284e
winningarc <shokim01@gmail.com>
2020-11-17 10:31:06 +0900

    fix .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,37 @@
 module.exports = {
-  "extends": [
-    "airbnb",
-    "plugin:prettier/recommended",
-    "prettier/react"
+  extends: [
+    // "airbnb",
+    'plugin:prettier/recommended',
+    'prettier/react',
   ],
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es6": true,
-    "jest": true,
-    "node": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
   },
-  "rules": {
-    "jsx-a11y/href-no-hash": ["off"],
-    "react/jsx-filename-extension": ["warn", { "extensions": [".js", ".jsx"] }],
-    "max-len": [
-      "warn",
+  rules: {
+    'jsx-a11y/href-no-hash': ['off'],
+    'max-len': [
+      'warn',
       {
-        "code": 75,
-        "tabWidth": 2,
-        "comments": 75,
-        "ignoreComments": false,
-        "ignoreTrailingComments": true,
-        "ignoreUrls": true,
-        "ignoreStrings": true,
-        "ignoreTemplateLiterals": true,
-        "ignoreRegExpLiterals": true
-      }
-    ]
-  }
-}
+        code: 75,
+        tabWidth: 2,
+        comments: 75,
+        ignoreComments: false,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+};
