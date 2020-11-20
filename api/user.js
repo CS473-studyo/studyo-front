09e8d393e1d4003b44af85d9eebd8363fc8c28f2
Sunjuhyeong <67995959+Sunjuhyeong@users.noreply.github.com>
2020-11-20 19:53:43 +0900

    Keyword

--- api/user.js
+++ api/user.js
@@ -9,3 +9,19 @@ export const login = ({ email, password }) =>
   auth.post(`/login`, { email, password });
 // export const check = () => auth.get(`/check`);
 // export const logout = () => auth.get(`/logout`);
+
+const auth_Keyword = axios.create({
+  baseURL: `http://localhost:8080/word`,
+  withCredentials: true,
+});
+
+export const save = ({ user, course, lecture, content }) =>
+  auth_Keyword.post(`/`, { user, course, lecture, content });
+
+export const getList = ({ course, lecture }) =>
+  auth_Keyword.post(`/list`, { course, lecture });
+  
+export const vote = (userId , { id1 }) =>
+auth_Keyword.post(`/join/${userId}`, { id:id1 });
+
+
