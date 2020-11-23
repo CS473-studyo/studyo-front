9c4de066c4c279c2aaff5704f0e6c08fc64e2c26
CheddarChoi <cde1103@kaist.ac.kr>
2020-11-23 20:04:42 +0900

    add footer component

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,12 @@
 import React, { useState } from 'react';
-import { Card } from 'react-bootstrap';
-import Header from 'components/header.js';
-import ClapButton from 'react-clap-button';
-import Clap from 'public/clap.svg';
+import Footer from 'components/footer';
 
 export default function Home() {
   const [totalCount, setTotalCount] = useState(0);
 
   return (
     <div className="pt-5">
-      <ClapButton
-        count={0}
-        countTotal={totalCount}
-        isClicked={false}
-        maxCount={3}
-        onCountChange={function onCountChange() {
-          setTotalCount((totalCount) => totalCount + 1);
-        }}
-        theme={{
-          secondaryColor: '#234382',
-        }}
-      />
-      <div>{totalCount}</div>
+      <Footer />
     </div>
   );
 }
