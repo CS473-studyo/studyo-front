14100d5ecab015a928edbc76defa9e8e35a46d8e
CheddarChoi <cde1103@kaist.ac.kr>
2020-11-24 11:00:16 +0900

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
+import Head from 'next/head';
 import '../public/custom_styles.scss';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/studyo-front/favicon.png"
+        />
+      </Head>
       <Component {...pageProps} />
     </>
   );
