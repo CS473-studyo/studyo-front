ff366d6a09acfea872c492a1d5ee76993e843c3c
winningarc <shokim01@gmail.com>
2020-11-24 13:43:00 +0900

    edit next config

--- next.config.js
+++ next.config.js
@@ -12,6 +12,6 @@ module.exports = {
 
     return config;
   },
-  assetPrefix: '/studyo-front/',
-  basePath: '/studyo-front',
+  // assetPrefix: '/studyo-front/',
+  // basePath: '/studyo-front',
 };
